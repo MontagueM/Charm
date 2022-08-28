99958bc6a19844af68d1c5ee5ab4f070b48eaba9
Montague <6136789+MontagueM@users.noreply.github.com>
2022-08-28 10:10:44 +0100

    New version

--- Charm/MainWindow.xaml.cs
+++ Charm/MainWindow.xaml.cs
@@ -95,7 +95,7 @@ public partial class MainWindow
 
     private async void CheckVersion()
     {
-        var currentVersion = new ApplicationVersion("1.2.2");
+        var currentVersion = new ApplicationVersion("1.2.3");
         var versionChecker = new ApplicationVersionChecker("https://github.com/MontagueM/Charm/raw/main/", currentVersion);
         versionChecker.LatestVersionName = "version";
         try
