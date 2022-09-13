ff95ee8bfb66762ac4fa980bd069d97069ee0500
Bytestorm HOME <gbytestorm@gmail.com>
2022-09-12 23:59:35 -0400

    remove temporary garbage from mat_to_blender.py

--- Field/import_mat_to_blender.py
+++ Field/import_mat_to_blender.py
@@ -28,6 +28,10 @@ def assemble_mat():
         variable_dict[var_name + ".y"] = y
         variable_dict[var_name + ".z"] = z
     
+    def addFloat2(var_name, x, y):
+        variable_dict[var_name + ".x"] = x
+        variable_dict[var_name + ".y"] = y
+    
     def addFloat(var_name, x):
         variable_dict[var_name + ".x"] = x
         
@@ -40,47 +44,22 @@ def assemble_mat():
     #Clamp: ShaderNodeClamp
     #Value: ShaderNodeValue
     
-    ### CBUFFERS ###
-    #static float4 cb0[10]
-
-    addFloat4('cb0[0]', 1, 2, 0, 0)
-    addFloat4('cb0[1]', 1.25, 0.33425003, 0.12500003, 1.5)
-    addFloat4('cb0[2]', -1.2214149, -0.17149174, -0.013661109, -1.4)
-    addFloat4('cb0[3]', 0.9, 1, 1, 1.225)
-    addFloat4('cb0[4]', -0.83695656, -0.87820184, -0.87820184, -0.975)
-    addFloat4('cb0[5]', 0.14658621, 0.13658907, 0.10114445, 0.35)
-    addFloat4('cb0[6]', 2, -2, 0, 0)
-    addFloat4('cb0[7]', 1.9921875, -1, 0.39999998, 0)
-    addFloat4('cb0[8]', 2.5, 5, 0, 0)
-    addFloat4('cb0[9]', 3.984375, -2, 0.897024, 0)
-
-    ### Function Definition ###
-    ### v[n] vars ###
-    addFloat4('v0', 1.0, 1.0, 1.0, 1.0)
-
-    addFloat4('v1', 1.0, 1.0, 1.0, 1.0)
-
-    addFloat4('v2', 1.0, 1.0, 1.0, 1.0)
-
-    addFloat4('v3', 1.0, 1.0, 1.0, 1.0)
-
-    addFloat3('v4', 1.0, 1.0, 1.0)
-
-    addFloat4('v5', 1.0, 1.0, 1.0, 1.0)
-
-    addFloat('v6', 1.0)
+    ### REPLACE WITH STATIC VARIABLES ###
     
     i = 0
-    for var in variable_dict.keys:
+    for var in variable_dict.keys():
         varNode = matnodes.new("ShaderNodeValue")
         varNode.location = (-370.0, 200.0 + (float(i)*-1.1)*50)
         varNode.label = var
-        varNode.value = variable_dict[var]
+        varNode.outputs[0].default_value = variable_dict[var]
+        varNode.hide = True
         variable_dict[var] = varNode
         i += 1
         print(var)
 
     #### REPLACE WITH SCRIPT ####
+    
+    
 
 def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
 
