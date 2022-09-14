b87f5734d3a46c2785e260f9f35470b446c06b64
Bytestorm HOME <gbytestorm@gmail.com>
2022-09-14 04:54:40 -0400

    started work on bringing the output script into bpy

--- Field/import_mat_to_blender.py
+++ Field/import_mat_to_blender.py
@@ -70,7 +70,98 @@ def assemble_mat():
 
     #### REPLACE WITH SCRIPT ####
     
-    
+    if True:        #Base Color (Albedo)
+        combineRGB = matnodes.new("ShaderNodecombineColor")
+        link(variable_dict['o0.x'], combineRGB.inputs[0])
+        link(variable_dict['o0.y'], combineRGB.inputs[1])
+        link(variable_dict['o0.z'], combineRGB.inputs[2])
+        link(combineRGB.outputs[0], principled_node.inputs[0])
+        
+    if True:        #Normal
+        biasSubtract1 = matnodes.new("ShaderNodeMath")
+        biasSubtract2 = matnodes.new("ShaderNodeMath")
+        biasSubtract3 = matnodes.new("ShaderNodeMath")
+        biasSubtract1.operation = 'SUBTRACT'
+        biasSubtract2.operation = 'SUBTRACT'
+        biasSubtract3.operation = 'SUBTRACT'
+        
+        link(variable_dict['o1.x'], biasSubtract1.inputs[0])
+        link(variable_dict['o1.y'], biasSubtract2.inputs[0])
+        link(variable_dict['o1.z'], biasSubtract3.inputs[0])        
+        
+        biasSubtract1.inputs[1].default_value = 0.5
+        biasSubtract2.inputs[1].default_value = 0.5
+        biasSubtract3.inputs[1].default_value = 0.5
+        
+        combineNormal = matnodes.new("ShaderNodeCombineXYZ")
+        link(biasSubtract1.outputs[0], combineNormal.inputs[0])
+        link(biasSubtract2.outputs[0], combineNormal.inputs[1])
+        link(biasSubtract3.outputs[0], combineNormal.inputs[2])
+        
+        normalLengthNode = matnodes.new("ShaderNodeVectorMath")
+        normalLengthNode.operation = 'LENGTH'
+        link(combineNormal.outputs[0], normalLengthNode.inputs[0])
+        variable_dict['normal_length_output'] = normalLengthNode.outputs[0]
+        
+        normalLengthInvert = matnodes.new("ShaderNodeMath")
+        normalLengthInvert.operation = 'DIVIDE'
+        normalLengthInvert.inputs[0].default_value = 1.0
+        link(normalLengthNode.outputs[0], normalLengthInvert.inputs[1])
+        
+        normal_in_world_space = matnodes.new("ShaderNodeVectorMath")
+        normal_in_world_space.operation = 'SCALE'
+        link(combineNormal.outputs[0], normal_in_world_space.inputs[0])
+        link(normalLengthInvert.outputs[0], normal_in_world_space.inputs[1])
+        
+        resplit = matnodes.new("ShaderNodeSeparateXYZ")
+        link(normal_in_world_space.outputs[0], resplit.inputs[0])
+        recombine = matnodes.new("ShaderNodeCombineXYZ")
+        link(resplit.outputs[0], recombine.inputs[0])
+        link(resplit.outputs[1], recombine.inputs[1])
+        
+        normal_dot = matnodes.new("ShaderNodeVectorMath")l
+        normal_dot.operation = 'DOT_PRODUCT'
+        link(recombine.outputs[0], normal_dot.inputs[0])
+        link(recombine.outputs[0], normal_dot.inputs[1])
+        
+        normal_saturate = matnodes.new("ShaderNodeClamp")
+        #existing defaults are fine
+        link(normal_dot.outputs[0], normal_saturate.inputs[0])
+        
+        normal_inverse = matnodes.new("ShaderNodeMath")
+        normal_inverse.operation = 'SUBTRACT'
+        normal_inverse.inputs[0].default_value = 1
+        link(normal_saturate.outputs[0], normal_inverse.inputs[1])
+        
+        normal_sqrt = matnodes.new("ShaderNodeMath")
+        normal_sqrt.operation = 'SQRT'
+        link(normal_inverse.outputs[0], normal_sqrt.inputs[0])
+        
+        full_recombine = matnodes.new("ShaderNodeCombineXYZ")
+        link(resplit.outputs[0], full_recombine.inputs[0])
+        link(resplit.outputs[1], full_recombine.inputs[1])
+        link(normal_sqrt.outputs[0], full_recombine.inputs[2])
+        
+        normal_post_1 = matnodes.new("ShaderNodeVectorMath")l
+        normal_post_1.operation = 'MULTIPLY_ADD'
+        link(full_recombine.outputs[0], normal_post_1.inputs[0])
+        normal_post_1.inputs[1].default_value = [2.0, 2.0, 2.0]
+        normal_post_1.inputs[2].default_value = [-1.5, -1.5, -1.5]
+        
+        normal_post_2 = matnodes.new("ShaderNodeVectorMath")l
+        normal_post_2.operation = 'MULTIPLY_ADD'
+        link(normal_post_1.outputs[0], normal_post_2.inputs[0])
+        normal_post_2.inputs[1].default_value = [0.5, 0.5, 0.5]
+        normal_post_2.inputs[2].default_value = [0.5, 0.5, 0.5]
+        
+        normal_post_norm = matnodes.new("ShaderNodeVectorMath")l
+        normal_post_norm.operation = 'NORMALIZE'
+        link(normal_post_2.outputs[0], normal_post_norm.inputs[0])
+        
+        link(normal_post_norm.outputs[0], principled_node.inputs[23])
+        
+    if True:
+        link(variable_dict['o2.x'], principled_node.inputs[6])
 
 def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
 
