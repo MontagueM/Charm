87918aaa78067b4ebe7644e03bc2f564c033f541
DeltaDesigns <50308149+DeltaDesigns@users.noreply.github.com>
2022-09-25 18:34:26 -0400

    ??

--- Charm/GeneralConfigView.xaml.cs
+++ Charm/GeneralConfigView.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using Field;
-using Field.Textures;
 
 namespace Charm;
 
--- Field/Textures/Material.cs
+++ Field/Textures/Material.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Field.Entities;
 using Field.General;
-using Field.Textures;
 using File = System.IO.File;
 
 namespace Field;
