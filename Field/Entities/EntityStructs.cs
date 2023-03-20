967778199fece7b0c0421920ae6f78cf7fca088b
DeltaDesigns <50308149+DeltaDesigns@users.noreply.github.com>
2023-03-20 18:35:10 -0400

    Fix broken api gstacks

--- Field/Entities/EntityStructs.cs
+++ Field/Entities/EntityStructs.cs
@@ -167,7 +167,7 @@ public class TexturePlate : Tag
         foreach (var transform in Header.PlateTransforms)
         {
             ScratchImage original = transform.Texture.GetScratchImage();
-            ScratchImage resizedOriginal = original.Resize(transform.Scale.X, transform.Scale.Y, 0);
+            ScratchImage resizedOriginal = original.Resize(transform.Scale.X, transform.Scale.Y, TEX_FILTER_FLAGS.SEPARATE_ALPHA);
             TexHelper.Instance.CopyRectangle(resizedOriginal.GetImage(0, 0, 0), 0, 0, transform.Scale.X, transform.Scale.Y, outputPlate.GetImage(0, 0, 0), bSrgb ? TEX_FILTER_FLAGS.SEPARATE_ALPHA : 0, transform.Translation.X, transform.Translation.Y);
             original.Dispose();
             resizedOriginal.Dispose();
