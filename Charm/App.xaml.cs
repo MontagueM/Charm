a210775b181805896463172381afab4a3dae2165
DeltaDesigns <50308149+DeltaDesigns@users.noreply.github.com>
2025-08-20 22:47:46 -0400

    Bump Version: 3.0.8

--- Charm/App.xaml.cs
+++ Charm/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Charm;
 /// </summary>
 public partial class App : Application
 {
-    public static ApplicationVersion CurrentVersion = new("3.0.7");
+    public static ApplicationVersion CurrentVersion = new("3.0.8");
 
     private void Application_Startup(object sender, StartupEventArgs e)
     {
