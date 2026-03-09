4f32fab57f1154cf2c799003a584d4970a7e38b7
DeltaDesigns <50308149+DeltaDesigns@users.noreply.github.com>
2026-03-09 15:43:35 -0400

    couple fixes
    installing .net10 messed up the solution on vs2022 as the global.json used the latest .net version installed instead

--- Charm/Views/LogView.xaml.cs
+++ Charm/Views/LogView.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Timers;
 using System.Windows;
 using System.Windows.Threading;
@@ -8,43 +7,63 @@ using UserControl = System.Windows.Controls.UserControl;
 
 namespace Charm;
 
+// TODO, Remove since LogView isnt even used anywhere anymore
+// the log file/cmd log is good enough
 public partial class LogView : UserControl
 {
-    private StringBuilder _logsBuffer = new();
-    private System.Timers.Timer _timer = new(2000);
+    //private StringBuilder _logsBuffer = new();
+    //private System.Timers.Timer _timer = new(2000);
 
     public LogView()
     {
         InitializeComponent();
 
-        Log.BindDelegate(OnLogEvent);
-        _timer.Elapsed += OnTimer;
-        _timer.Start();
+        //Log.BindDelegate(OnLogEvent);
+        //_timer.Elapsed += OnTimer;
+        //_timer.Start();
     }
 
     // todo amortize this, as can cause huge thread issues when a threaded process logs a call when the receiver
     // requires a ui thread dispatch (or just some way to stop this being terrible)
     private void OnLogEvent(object? sender, LogEventArgs e)
     {
-        if (e.Verbosity > LogVerbosity.Info)
-        {
-            return;
-        }
+        //if (e.Verbosity > LogVerbosity.Info)
+        //{
+        //    return;
+        //}
 
-        _logsBuffer.AppendLine(e.Message);
+        //_logsBuffer.AppendLine(e.Message);
     }
 
     private void OnTimer(object? sender, ElapsedEventArgs elapsedEventArgs)
     {
-        Dispatcher.Invoke(() =>
-        {
-            LogBox.AppendText(_logsBuffer.ToString());
-            LogBox.ScrollToEnd();
-        });
-        _logsBuffer.Clear();
-        _timer.Start();
+        //if (Dispatcher.HasShutdownStarted || Dispatcher.HasShutdownFinished)
+        //{
+        //    _timer.Stop();
+        //    _timer.Elapsed -= OnTimer;
+        //    return;
+        //}
+
+        //try
+        //{
+        //    Dispatcher.InvokeAsync(() =>
+        //    {
+        //        if (_logsBuffer.Length == 0) return;
+        //        LogBox.AppendText(_logsBuffer.ToString());
+        //        LogBox.ScrollToEnd();
+        //        _logsBuffer.Clear();
+        //    }, DispatcherPriority.Background);
+        //}
+        //catch (TaskCanceledException)
+        //{
+        //    _timer.Stop();
+        //    _timer.Elapsed -= OnTimer;
+        //}
+        //_logsBuffer.Clear();
+        //_timer.Start();
     }
 }
+
 
 public class LogHandler
 {
